191749b38bed432dbd7afc661185efcb4bab8626
eliel <jviviant@exa.unicen.edu.ar>
2020-09-07 11:46:44 -0300

    matriz declarada

--- src/compiladores/AnalizadorLexico/AnalizadorLexico.java
+++ src/compiladores/AnalizadorLexico/AnalizadorLexico.java
@@ -2,7 +2,7 @@ package compiladores.AnalizadorLexico;
 
 public class AnalizadorLexico {
 
-    private MatrizDeTransicion automata[][]= new MatrizDeTransicion[12][];
+    private MatrizDeTransicion automata[][]= new MatrizDeTransicion[17][26];
 
 
 }
