a931d82e5d2fca33f2e7dbed06112a7cbc66c158
Matias Morandeira <morandeiramatias@gmail.com>
2020-10-05 19:49:42 -0300

    Fix script CompilarGramatica.sh.

--- src/resources/compilarGramatica.sh
+++ src/resources/compilarGramatica.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 yacc -J gramatica.y
-sed -i '1s/^/package compiladores;\n/' ParserVal.java
-mv ./Parser.java ./src/compiladores
-mv ./ParserVal.java ./src/compiladores
+sed -i '1s/^/package Compilador.Sintactico;\n/' ParserVal.java
+mv ./Parser.java ..\main\java\Compilador\Sintactico\
+mv ./ParserVal.java ..\main\java\Compilador\Sintactico\
