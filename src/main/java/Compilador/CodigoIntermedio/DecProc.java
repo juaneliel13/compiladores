32d84de5db09ba05ec6ced6cbfd573715373dba9
eliel <jviviant@exa.unicen.edu.ar>
2020-11-12 19:55:13 -0300

    se agregó la referencia al procedimiento

--- src/main/java/Compilador/CodigoIntermedio/DecProc.java
+++ src/main/java/Compilador/CodigoIntermedio/DecProc.java
@@ -2,8 +2,10 @@ package Compilador.CodigoIntermedio;
 
 public class DecProc extends Nodo{
 
-    public DecProc(Nodo izquierdo, Nodo derecho) {
+    String ref;
+    public DecProc(Nodo izquierdo, Nodo derecho, String ref) {
         super(izquierdo, derecho);
+        this.ref=ref;
     }
 
     @Override
