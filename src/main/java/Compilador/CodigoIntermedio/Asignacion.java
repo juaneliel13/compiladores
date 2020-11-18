f0ed51f12e8877cd827491b7cf9be48d69b79ddb
Matias Morandeira <morandeiramatias@gmail.com>
2020-11-18 12:38:06 -0300

    Se arreglo asignacion cuando el lado derecho es una hoja.

--- src/main/java/Compilador/CodigoIntermedio/Asignacion.java
+++ src/main/java/Compilador/CodigoIntermedio/Asignacion.java
@@ -1,23 +1,24 @@
 package Compilador.CodigoIntermedio;
 
+import Compilador.CodigoAssembler.AdministradorDeRegistros;
 import Compilador.Lexico.Tipos;
 
 public class Asignacion extends ConTipo {
     static Tipos[][] compatibilidad = new Tipos[3][3];
 
     static {
-        for(int i=0;i<compatibilidad.length;i++)
-            for(int j=0;j<compatibilidad[0].length;j++){
-                if(i==j)
-                    compatibilidad[i][j]=Tipos.valueOf(i);
+        for (int i = 0; i < compatibilidad.length; i++)
+            for (int j = 0; j < compatibilidad[0].length; j++) {
+                if (i == j)
+                    compatibilidad[i][j] = Tipos.valueOf(i);
                 else
-                    compatibilidad[i][j]=null;
+                    compatibilidad[i][j] = null;
             }
     }
 
     public Asignacion(ConTipo izquierdo, ConTipo derecho) {
         super(izquierdo, derecho);
-        if(izquierdo==null || izquierdo.getTipo()==null || derecho==null || derecho.getTipo()==null)
+        if (izquierdo == null || izquierdo.getTipo() == null || derecho == null || derecho.getTipo() == null)
             setTipo(null);
         else {
             setTipo(compatibilidad[izquierdo.getTipo().getValue()][derecho.getTipo().getValue()]);
@@ -31,15 +32,24 @@ public class Asignacion extends ConTipo {
         derecho.generarCodigo();
         ConTipo izq = (ConTipo) izquierdo;
         ConTipo der = (ConTipo) derecho;
-        if(getTipo()==Tipos.INTEGER){
+        if (getTipo() == Tipos.INTEGER) {
+            if (der.esHoja()) {
+                this.reg = AdministradorDeRegistros.get16bits(this);
+                codigo.append("MOV ");
+                codigo.append(this.reg);
+                codigo.append(",");
+                codigo.append(der.getRef());
+                codigo.append("\n");
+            } else {
+                this.reg = der.reg;
+            }
             codigo.append("MOV ");
             codigo.append(izq.getRef());
             codigo.append(",");
-            codigo.append(der.getRef());
+            codigo.append(this.reg);
             codigo.append("\n");
-            if(!derecho.esHoja())
-                der.reg.liberar();
-        }else{
+            this.reg.liberar();
+        } else {
             codigo.append("FLD ");
             codigo.append(der.getRef());
             codigo.append("\n");
