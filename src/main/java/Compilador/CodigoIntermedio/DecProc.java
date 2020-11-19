472fef3f014d0749aecb3997d863fbc2672a2bf1
elielviviant <jviviant@exa.unicen.edu.ar>
2020-11-19 14:03:10 -0300

    se agregó la declaracion de procedimientos al assembler

--- src/main/java/Compilador/CodigoIntermedio/DecProc.java
+++ src/main/java/Compilador/CodigoIntermedio/DecProc.java
@@ -1,6 +1,13 @@
 package Compilador.CodigoIntermedio;
 
+import Compilador.Lexico.Tipos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DecProc extends Nodo{
+
+    public static StringBuilder procs = new StringBuilder();
 
     String ref;
     public DecProc(Nodo izquierdo, Nodo derecho, String ref) {
@@ -10,5 +17,17 @@ public class DecProc extends Nodo{
 
     @Override
     public void generarCodigo() {
+        StringBuilder aux = Nodo.codigo;
+        Nodo.codigo = procs;
+        codigo.append("_"+ref);
+        codigo.append(" PROC ");
+        codigo.append("\n");
+        izquierdo.generarCodigo();
+        codigo.append("RET");
+        codigo.append("\n");
+        codigo.append("_"+ref);
+        codigo.append(" ENDP");
+        codigo.append("\n");
+        Nodo.codigo = aux;
     }
 }
