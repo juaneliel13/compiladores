4bf2db991efcf3e7a43ede887c5084ee41599d4d
Matias Morandeira <morandeiramatias@gmail.com>
2020-11-20 19:51:52 -0300

    Se acomodo el codigo de Comparador para que sea mas legible.

--- src/main/java/Compilador/CodigoIntermedio/Comparador.java
+++ src/main/java/Compilador/CodigoIntermedio/Comparador.java
@@ -16,49 +16,80 @@ public abstract class Comparador extends Operador {
         derecho.generarCodigo();
         ConTipo izq = (ConTipo) izquierdo;
         ConTipo der = (ConTipo) derecho;
-        String reg1, reg2;
-        reg2=der.getRef();
 
+        //Genero el codigo de la comparacion de los numeros
         if(izq.getTipo() == Tipos.INTEGER) {
-            if (izquierdo.esHoja() && derecho.esHoja()) {
-                this.reg = AdministradorDeRegistros.get16bits(this);
-                codigo.append("MOV ");
-                codigo.append(reg);
-                codigo.append(",");
-                codigo.append(izq.getRef());
-                codigo.append("\n");
-                reg1 = this.reg.toString();
-
-            } else {
-                reg1 = izq.getRef();
-            }
-            codigo.append("CMP ");
-            codigo.append(reg1);
-            codigo.append(",");
-            codigo.append(reg2);
-            codigo.append("\n");
-            reg.liberar();
+            comparadorInteger(izq,der);
         }
-        else if(izq.getTipo() == Tipos.FLOAT) //esto por ahi es al pedo ya que no tenemos otros tipos o si hay error no
-                                              //se llama a generar codigo
+        else
         {
-            //si no existe mem2bytes
-                //la creo
-            codigo.append("FLD ");// op1
-            codigo.append(izq.getRef());
-            codigo.append("\nFCOM ");//op2
-            codigo.append(der.getRef());
-            codigo.append("\nFSTSW mem2bytes\nMOV AX,mem2bytes\nSAHF\n");
+            //Al solo tener dos tipos y solo llamarlo a generar codigo
+            //cuando no hay errores en este caso seria un Float
+            comparadorFloat(izq,der);
         }
-        String etiqueta = crearEtiqueta();
 
+        //Creo la etiqueta y agrego el codigo para saltar
+        String etiqueta = crearEtiqueta();
+        apilar(etiqueta);
         codigo.append(getSalto());
         codigo.append(" ");
         codigo.append(etiqueta);
         codigo.append("\n");
-
-        apilar(etiqueta);
     }
 
+
+    /**
+     * Metodo templado para obtener la instruccion de salto segun el tipo de Comparador concreto
+     * @return devuelve la instruccion de salto.
+     */
     protected abstract String getSalto();
+
+
+    /**
+     * Genera el codigo para comparar dos Integer.
+     * @param izq Nodo izquierdo de la comparacion
+     * @param der Nodo derecho de la comparacion
+     */
+    private void comparadorInteger(ConTipo izq, ConTipo der) {
+        String reg1, reg2;
+        reg2=der.getRef();
+        if (izq.esHoja() && der.esHoja()) {
+            this.reg = AdministradorDeRegistros.get16bits(this);
+            codigo.append("MOV ");
+            codigo.append(reg);
+            codigo.append(",");
+            codigo.append(izq.getRef());
+            codigo.append("\n");
+            reg1 = this.reg.toString();
+        } else {
+            reg1 = izq.getRef();
+        }
+
+        //genero la comparacion
+        codigo.append("CMP ");
+        codigo.append(reg1);
+        codigo.append(",");
+        codigo.append(reg2);
+        codigo.append("\n");
+
+        //libero el registro
+        reg.liberar();
+    }
+
+
+    /**
+     * Genera el codigo para comparar dos Floats.
+     * @param izq Nodo izquierdo de la comparacion
+     * @param der Nodo derecho de la comparacion
+     */
+    private void comparadorFloat(ConTipo izq, ConTipo der) {
+        //si no existe mem2bytes
+        //la creo
+        codigo.append("FLD ");// op1
+        codigo.append(izq.getRef());
+        codigo.append("\nFCOM ");//op2
+        codigo.append(der.getRef());
+        codigo.append("\nFSTSW mem2bytes\nMOV AX, mem2bytes\nSAHF\n");
+    }
+
 }
