21dc29c70b6403aab6b9eaf8c1c9ffed1c16b81b
Matias Morandeira <morandeiramatias@gmail.com>
2020-11-21 16:00:04 -0300

    Se comento el codigo de For.

--- src/main/java/Compilador/CodigoIntermedio/For.java
+++ src/main/java/Compilador/CodigoIntermedio/For.java
@@ -8,19 +8,34 @@ public class For extends Nodo {
 
     @Override
     public void generarCodigo() {
+
+        //      <etiqueta_for>
+        //------------------------------------------------------+ codigo generado por izquierdo.generarCodigo();
+        //      condicion                                       + (que es un nodo Comparador)
+        //      JF <etiqueta_desapilada>                        +
+        //------------------------------------------------------+
+        //      ... (bloque del for)
+        //      JMP <etiqueta_for>
+        //      <etiqueta_desapilada>
+
+        //Se crea la etiqueta_for y se agrega
         String etiqueta_for = crearEtiqueta();
         codigo.append(etiqueta_for);
         codigo.append(":\n");
+
+        //Se genera el codigo de la condicion (izquierdo) y luego el cuerpo del for (derecho)
         izquierdo.generarCodigo();
         derecho.generarCodigo();
+
+        //Se agrega el salto incodicional a la condicion
         codigo.append("JMP ");
         codigo.append(etiqueta_for);
         codigo.append("\n");
+
+        //Se agrega la etiqueta si no se cumple la condicon para salir del bucle
         codigo.append(desapilar());
         codigo.append(":\n");
-
-
-
-
     }
+
+
 }
