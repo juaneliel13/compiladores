38e4ae7e8d7bcce31f15a647e20f2e894b804c5c
Matias Morandeira <morandeiramatias@gmail.com>
2020-11-21 10:59:29 -0300

    Se acomodo y comento DecProc. Tambien se arreglo error de anidamiento de los procedimientos.

--- src/main/java/Compilador/CodigoIntermedio/DecProc.java
+++ src/main/java/Compilador/CodigoIntermedio/DecProc.java
@@ -2,14 +2,11 @@ package Compilador.CodigoIntermedio;
 
 import Compilador.Lexico.Tipos;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DecProc extends Nodo{
 
     public static StringBuilder procs = new StringBuilder();
-
-
 
     String ref;
     public DecProc(Nodo izquierdo, Nodo derecho, String ref) {
@@ -19,22 +16,60 @@ public class DecProc extends Nodo{
 
     @Override
     public void generarCodigo() {
-        StringBuilder aux = Nodo.codigo;
+
+        //Se crea un nuevo StringBuilder para guardar el codigo de este PROC
+        StringBuilder aux = new StringBuilder();
+        StringBuilder save_nodo = Nodo.codigo;
+        Nodo.codigo = aux;
+
+        //Genero la variable para controlar la recursion en ejecucion
+        generarFlagRecursion();
+
+        //Genero la etiqueta del procedimiento
+        generarEtiquetaProc();
+
+        //Genero el codigo del procedimiento
+        izquierdo.generarCodigo();
+
+        //Agrego el RET
+        generarRet();
+
+        //Agrego este procedimiento al procs
+        procs.append(aux.toString());
+
+        //Restauro el StringBuilder del nodo
+        Nodo.codigo = save_nodo;
+    }
+
+    /**
+     * Agrega una variable a la tabla de simbolos
+     * para controlar la recursion de los procedimientos
+     */
+    private void generarFlagRecursion() {
         HashMap<String, Object> h_aux = new HashMap<String, Object>();
         h_aux.put("Uso", "variable");
         h_aux.put("Tipo",Tipos.INTEGER);
         h_aux.put("Inic","0");
         lex.tablaDeSimbolos.put("FLAG_"+ref, h_aux);
-        Nodo.codigo = procs;
-        codigo.append("_"+ref);
-        codigo.append(" PROC ");
-        codigo.append("\n");
-        izquierdo.generarCodigo();
-        codigo.append("RET");
-        codigo.append("\n");
-        codigo.append("_"+ref);
-        codigo.append(" ENDP");
-        codigo.append("\n");
-        Nodo.codigo = aux;
     }
+
+    /**
+     * Genera la etiqueta del procedimiento
+     */
+    private void generarEtiquetaProc() {
+        codigo.append("_");
+        codigo.append(ref);
+        codigo.append(" PROC\n");
+    }
+
+    /**
+     * Genera el RET
+     */
+    private void generarRet() {
+        codigo.append("RET\n");
+        codigo.append("_");
+        codigo.append(ref);
+        codigo.append(" ENDP\n");
+    }
+
 }
