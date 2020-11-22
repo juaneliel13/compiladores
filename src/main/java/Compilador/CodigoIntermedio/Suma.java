39c94d29504629a69cced9bb69f243ea1bb0dd37
Matias Morandeira <morandeiramatias@gmail.com>
2020-11-22 14:13:25 -0300

    Se comento y acomodo el codigo de Suma.

--- src/main/java/Compilador/CodigoIntermedio/Suma.java
+++ src/main/java/Compilador/CodigoIntermedio/Suma.java
@@ -16,54 +16,64 @@ public class Suma extends Operador {
         ConTipo izq = (ConTipo) izquierdo;
         ConTipo der = (ConTipo) derecho;
         if (this.getTipo() == Tipos.INTEGER) {
-            String reg1, reg2;
-            if (izquierdo.esHoja() && derecho.esHoja()) {
-                reg = AdministradorDeRegistros.get16bits(this);
-                codigo.append("MOV ");
-                codigo.append(reg);
-                codigo.append(",");
-                codigo.append(izq.getRef());
-                codigo.append("\n");
-                reg1 = reg.toString();
-                reg2 = der.getRef();
-            } else {
-                if (izquierdo.esHoja()) {
-                    reg = der.reg;
-                    AdministradorDeRegistros.nombre.put(reg,this);
-                    reg1 = der.getRef();
-                    reg2 = izq.getRef();
-                } else {
-                    reg = izq.reg;
-                    AdministradorDeRegistros.nombre.put(reg,this);
-                    reg1 = izq.getRef();
-                    reg2 = der.getRef();
-                }
-                if (!izquierdo.esHoja() && !derecho.esHoja()) {
-                    der.reg.liberar();
-                }
-
-            }
-            codigo.append("ADD ");
-            codigo.append(reg1);
-            codigo.append(",");
-            codigo.append(reg2);
-            codigo.append("\n");
+            sumaInteger(izq,der);
         } else {
-            codigo.append("FLD ");
-            codigo.append(izq.getRef());
-            codigo.append("\n");
-            codigo.append("FADD ");
-            codigo.append(der.getRef());
-            codigo.append("\n");
-
-            String aux = crearAuxiliar();
-            codigo.append("FSTP ");
-            codigo.append(aux);
-            codigo.append("\n");
-            var_aux = aux;
-            //generacion de codigo para suma flotante
+            sumaFloat(izq,der);
         }
     }
 
+    /**
+     * Esta funcion genera el codigo assembler para la suma de izq + der de INTEGER.
+     *
+     * @param izq Nodo ConTipo que representa el primer operando
+     * @param der Nodo ConTipo que representa el segundo operando
+     */
+    private void sumaInteger(ConTipo izq, ConTipo der) {
+        String reg1, reg2;
+        if (izquierdo.esHoja() && derecho.esHoja()) {
+            moverHojaAReg(izq);
+            reg1 = reg.toString();
+            reg2 = der.getRef();
+        } else {
+            if (izquierdo.esHoja()) {
+                reg = der.reg;
+                reg1 = der.getRef();
+                reg2 = izq.getRef();
+            } else {
+                reg = izq.reg;
+                reg1 = izq.getRef();
+                reg2 = der.getRef();
+            }
+            AdministradorDeRegistros.nombre.put(reg,this);
+            if (!izquierdo.esHoja() && !derecho.esHoja()) {
+                der.reg.liberar();
+            }
+        }
+        codigo.append("ADD ");
+        codigo.append(reg1);
+        codigo.append(",");
+        codigo.append(reg2);
+        codigo.append("\n");
+    }
 
+    /**
+     * Esta funcion genera el codigo assembler para la suma de izq + der de FLOAT.
+     *
+     * @param izq Nodo ConTipo que representa el primer operando
+     * @param der Nodo ConTipo que representa el segundo operando
+     */
+    private void sumaFloat(ConTipo izq, ConTipo der) {
+        codigo.append("FLD ");
+        codigo.append(izq.getRef());
+        codigo.append("\n");
+        codigo.append("FADD ");
+        codigo.append(der.getRef());
+        codigo.append("\n");
+
+        String aux = crearAuxiliar();
+        codigo.append("FSTP ");
+        codigo.append(aux);
+        codigo.append("\n");
+        var_aux = aux;
+    }
 }
